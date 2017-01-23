984ca959ceb4bdd57fc8e8a04367571354a1bf7c
Xavier DEBOUDT <xdeboudt@wanadoo.fr>
2017-01-23 14:49:46 +0100

    test du coucou

---
+++ src/CoucouI.java
@@ -0,0 +1,20 @@
+
+public class CoucouI {
+
+	private String coucou;
+
+	public CoucouI(String coucou) {
+		super();
+		this.coucou = coucou;
+	}
+
+	public String getCoucou() {
+		return coucou;
+	}
+
+	public void setCoucou(String coucou) {
+		this.coucou = coucou;
+	}
+	
+	
+}
